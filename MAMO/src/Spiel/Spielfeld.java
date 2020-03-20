499955d69334fa0e2be213d6788bb77152609e61
Marco Laengin <1922867@stud.hs-mannheim.de>
2020-03-20 16:59:31 +0100

    test mergen

--- MAMO/src/Spiel/Spielfeld.java
+++ MAMO/src/Spiel/Spielfeld.java
@@ -3,7 +3,7 @@ package Spiel;
 public class Spielfeld {
 
 	public static void main(String[] args) {
-		int variabelntest = 5;
+		int hallo = 5;
 		String testmerge = "hallo";
 		//long c = 20000;
 		//TEST
