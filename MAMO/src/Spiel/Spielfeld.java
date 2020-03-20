0e02d3daaef0c760bbd2beb2a784b2b503d9a9c8
Sven <Sven@DESKTOP-BNJVNA7.fritz.box>
2020-03-20 15:59:45 +0100

    Aufräumen

--- MAMO/src/Spiel/Spielfeld.java
+++ MAMO/src/Spiel/Spielfeld.java
@@ -3,9 +3,9 @@ package Spiel;
 public class Spielfeld {
 
 	public static void main(String[] args) {
-		int a = 5;
-		String b = "hallo";
-		long c = 20000;
+		//int a = 5;
+		//String b = "hallo";
+		//long c = 20000;
 
 	}
 
